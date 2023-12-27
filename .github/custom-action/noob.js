530168965ada87b9de857673aa6213e473935e01
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-12-27 10:13:17 -0600

    Fixed Typo

--- .github/custom-action/noob.js
+++ .github/custom-action/noob.js
@@ -2,9 +2,9 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 
 function run() {
-    const name: core.getInput('name', {required: true});
-    const age: core.getInput('age', {required: true});
-    const occupation: core.getInput('occupation', {required: true});
+    const name = core.getInput('name', {required: true});
+    const age = core.getInput('age', {required: true});
+    const occupation = core.getInput('occupation', {required: true});
 
     const message = `Name: ${name}\n
                      Age: ${age}\n
