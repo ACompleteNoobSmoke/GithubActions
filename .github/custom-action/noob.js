30809ddc6249d99ace21cf8e053cad8cd388f421
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-12-27 10:09:23 -0600

    Modified Action

--- .github/custom-action/noob.js
+++ .github/custom-action/noob.js
@@ -1,10 +1,16 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
-console.log("Hello World")
-
 function run() {
-    core.setOutput('message', 'Osaretin Is The Man');
+    const name: core.getInput('name', {required: true});
+    const age: core.getInput('age', {required: true});
+    const occupation: core.getInput('occupation', {required: true});
+
+    const message = `Name: ${name}\n
+                     Age: ${age}\n
+                     Occupation: ${occupation}`
+
+    core.setOutput('message', message);
 }
 
 run();
